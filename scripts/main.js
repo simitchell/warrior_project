7ce8244343be087990778f4d89c9314c3eb85348
Sam Mitchell <samuel.i.mitchell@gmail.com>
2023-09-20 23:24:44 -0400

    finished project

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,5 @@
 'use strict'
 
-
 // WARRIOR CLASS
 class Warrior {
   constructor(characterName, health = [], power) {
@@ -10,7 +9,6 @@ class Warrior {
   }
 }
 
-
 // Hero Class, an extension of Warrior
 class Hero extends Warrior {
   constructor(characterName, health = [], power, hasMjolnir) {
@@ -19,13 +17,11 @@ class Hero extends Warrior {
   }
 
   heroGreet(otherPerson) {
-    // console.log(`Hello ${otherPerson.characterName}, I am ${this.characterName} the Asgardian god of Thudner!  Your evil ways will abruptly come to an end!`);
-    document.querySelector('#hGreeting').innerText = `Hello ${otherPerson.characterName}, I am ${this.characterName} the Asgardian god of Thudner!  Your evil ways will abruptly come to an end!`;
+    document.querySelector('#hGreeting').innerText = `I am ${this.characterName} the Asgardian god of Thunder!  My power level is 15, my health is 30, and I wield Mjolnir!  None can defeat me, and if you should try, then your evil ways will abruptly come to an end!`;
   };
 
   heroAnnounce(otherPerson) {
-    // console.log(`Vile ${otherPerson.characterName}, you will now feel the unhindered power of Mjolnir.  Mjolnir!  Activate rm -rf!`);
-    document.querySelector('#hAnnounce').innerText = `Vile ${otherPerson.characterName}, you will now feel the unhindered power of Mjolnir.  Mjolnir!  Activate rm -rf!`;
+    document.querySelector('#hAnnounce').innerText = `Puny!?  I laugh at your ignorance.  See the power of Asgard in the strengh of my arms which are the size of Volkswagens.  Surely I do not concede this battle.  Vile ${otherPerson.characterName}, you will now feel the unhindered power of Mjolnir.  Mjolnir!  Activate rm -rf!`;
   };
 
   heroAttack(otherPerson) {
@@ -49,13 +45,11 @@ class Villain extends Warrior {
   }
 
   villainGreet(otherPerson) {
-    // console.log(`Greetings ${otherPerson.characterName}, I am mighty ${this.characterName}.  My power knows no bounds you puny thunder God.  I hope you are prepared for your inevitable death!`);
-    document.querySelector('#vGreeting').innerText = `Greetings ${otherPerson.characterName}, I am mighty ${this.characterName}.  My power knows no bounds you puny thunder God.  I hope you are prepared for your inevitable death!`;
+    document.querySelector('#vGreeting').innerText = `I am mighty ${this.characterName}.  With a power level of 10 and a health of 15, I stand in defiance of lesser beings who have spent lifetimes trying to unlock my weaknesses in vain.  My power knows no bounds!  You puny thunder god, I hope you are prepared for your inevitable death!  Concede now or I shall take it upon me to bring it thusly.`;
   }
 
   villainTaunt(otherPerson) {
-    // console.log(`You are gravely mistaken, young ${otherPerson.characterName}.  Now shudder in terror, for when my DOCUMENT OBJECT MODEL refreshes this universe you will disappear!`);
-    document.querySelector('#vTaunt').innerText = `You are gravely mistaken, young ${otherPerson.characterName}.  Now shudder in terror, for when my DOCUMENT OBJECT MODEL refreshes this universe you will disappear!`;
+    document.querySelector('#vTaunt').innerText = `You are gravely mistaken, young ${otherPerson.characterName}.  Now shudder in terror, for when my DOCUMENT OBJECT MODEL refreshes this universe you will disappear and Asgard will be forgotten for all time!`;
   }
 
   villainAttack(otherPerson) {
@@ -69,27 +63,7 @@ class Villain extends Warrior {
 const thor = new Hero('Thor', 30, 15, true);
 const javascript = new Villain('JavaScript', 25, 10, true);
 
-// Console log to confirm their accuracy
-// console.log(thor);
-// console.log(javascript);
-
-// Greetings to one another in preparation for battle
-// thor.heroGreet(javascript);
-// javascript.villainGreet(thor);
-
-// Hero announces himself, Villain taunts Hero
-// thor.heroAnnounce(javascript);
-// javascript.villainTaunt(thor);
-
-// Hero attacks Villain
-// thor.heroAttack(javascript);
-
-// Villain attacks Hero
-// javascript.villainAttack(thor);
-
-// Hero attacks Villain again
-// thor.heroAttack2(javascript);
-
+// Assign buttons to constants
 const btn0 = document.getElementById('btn0');
 const btn1 = document.getElementById('btn1');
 const btn2 = document.getElementById('btn2');
@@ -98,28 +72,59 @@ const btn4 = document.getElementById('btn4');
 const btn5 = document.getElementById('btn5');
 const btn6 = document.getElementById('btn6');
 
+// The actions behind button clicks.  Calls functions, reveals the next button in the story
 btn0.addEventListener('click', () => {
   thor.heroGreet(javascript);
+  function show(a) {
+    const element = document.getElementById('btn1');
+    element.classList.toggle(a);
+  }
+  show('btnVG');
 });
 
 btn1.addEventListener('click', () => {
   javascript.villainGreet(thor);
+  function show(a) {
+    const element = document.getElementById('btn2');
+    element.classList.toggle(a);
+  }
+  show('btnHAn')
 });
 
 btn2.addEventListener('click', () => {
   thor.heroAnnounce(javascript);
+  function show(a) {
+    const element = document.getElementById('btn3');
+    element.classList.toggle(a);
+  }
+  show('btnVT')
 });
 
 btn3.addEventListener('click', () => {
   javascript.villainTaunt(thor);
+  function show(a) {
+    const element = document.getElementById('btn4');
+    element.classList.toggle(a);
+  }
+  show('btnHAt')
 });
 
 btn4.addEventListener('click', () => {
   thor.heroAttack(javascript);
+  function show(a) {
+    const element = document.getElementById('btn5');
+    element.classList.toggle(a);
+  }
+  show('btnVAt')
 });
 
 btn5.addEventListener('click', () => {
   javascript.villainAttack(thor);
+  function show(a) {
+    const element = document.getElementById('btn6');
+    element.classList.toggle(a);
+  }
+  show('btnHSA')
 });
 
 btn6.addEventListener('click', () => {
