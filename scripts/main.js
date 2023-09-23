ab29e95a595c38953048f53e0bab966de4b74ec1
Sam Mitchell <samuel.i.mitchell@gmail.com>
2023-09-23 18:33:13 -0400

    added audio files and display of myHero myVillain upon select

--- scripts/main.js
+++ scripts/main.js
@@ -126,48 +126,21 @@ const btn3 = document.getElementById('btn3');
 const btn4 = document.getElementById('btn4');
 const btn5 = document.getElementById('btn5');
 
-// btnSubmit instantiates all heroes and villains, then selects them depending on the user's input
 let myHero;
 let myVillain;
 let myZombie;
+
+let snd = document.getElementById('konamiCode');
+let bgm = document.getElementById('bgm');
+
+// btnSubmit instantiates all heroes and villains, then selects them depending on the user's input, displays myHero and myVillain on screen
+
 btnSubmit.addEventListener('click', () => {
   event.preventDefault();
-  function selHero() {
-    const thor = new Hero('Thor', 'Asgardian god of Thunder', 30, 15, 'Mjolnir');
-    const sam = new Hero('Sam', 'Developer of Web', 35, 5, 'MacBook Pro');
-    const frodo = new Hero('Frodo Baggins', 'Ring-bearer', 60, 35, 'The One Ring')
-    if (document.querySelector('#heroSelect').value === "1") {
-      myHero = thor;
-    }
-    if (document.querySelector('#heroSelect').value === "2") {
-      myHero = sam;
-    }
-    if (document.querySelector('#heroSelect').value === "3") {
-      myHero = frodo;
-    }
-    console.log(myHero);
-    return myHero;
-  }
-
-  function selVillain() {
-    const javascript = new Villain('JavaScript', 'complicated', 30, 10, 'DOCUMENT OBJECT MODEL');
-    const zombie = new Villain('The Mysterious Undead', null, Infinity, 2, null);
-    const vader = new Villain('Darth Vader', 'Sith Lord', 60, 25, 'Force Choke');
-    if (document.querySelector('#villainSelect').value === "1") {
-      myVillain = javascript;
-    }
-    if (document.querySelector('#villainSelect').value === "2") {
-      myVillain = zombie;
-    }
-    if (document.querySelector('#villainSelect').value === "3") {
-      myVillain = vader;
-    }
-    console.log(myVillain);
-    return myVillain
-  }
-
   selHero();
   selVillain();
+  displayOutput();
+  playSound();
 });
 
 // btn0 through btn5 are clicked to proceed in the story.  They trigger text and the next consecutive button to display on the DOM
@@ -230,3 +203,81 @@ btn5.addEventListener('click', () => {
   event.preventDefault();
   myVillain.villainAttack(myHero);
 });
+
+// displays myHero and myVillain to the screen, visual confirmation of user's choices
+function displayOutput() {
+  document.getElementById('hOutput').innerText = `${myHero.characterName}`;
+  document.getElementById('vOutput').innerText = `${myVillain.characterName}`;
+  document.getElementById('vs').innerText = "vs.";
+};
+// sets user's input as myHero
+function selHero() {
+  const thor = new Hero('Thor', 'Asgardian god of Thunder', 30, 15, 'Mjolnir');
+  const sam = new Hero('Sam', 'Developer of Web', 35, 5, 'MacBook Pro');
+  const frodo = new Hero('Frodo Baggins', 'Ring-bearer', 60, 35, 'The One Ring')
+  if (document.querySelector('#heroSelect').value === "1") {
+    myHero = thor;
+  }
+  if (document.querySelector('#heroSelect').value === "2") {
+    myHero = sam;
+  }
+  if (document.querySelector('#heroSelect').value === "3") {
+    myHero = frodo;
+  }
+  console.log(myHero);
+  return myHero;
+}
+// sets user's input as myVillain
+function selVillain() {
+  const javascript = new Villain('JavaScript', 'complicated', 30, 10, 'DOCUMENT OBJECT MODEL');
+  const zombie = new Villain('The Mysterious Undead', null, Infinity, 2, null);
+  const vader = new Villain('Darth Vader', 'Sith Lord', 60, 25, 'Force Choke');
+  if (document.querySelector('#villainSelect').value === "1") {
+    myVillain = javascript;
+  }
+  if (document.querySelector('#villainSelect').value === "2") {
+    myVillain = zombie;
+  }
+  if (document.querySelector('#villainSelect').value === "3") {
+    myVillain = vader;
+  }
+  console.log(myVillain);
+  return myVillain
+}
+
+// ?
+var pattern = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a'];
+var current = 0;
+
+var keyHandler = function (event) {
+
+	// If the key isn't in the pattern, or isn't the current key in the pattern, reset
+	if (pattern.indexOf(event.key) < 0 || event.key !== pattern[current]) {
+		current = 0;
+		return;
+	}
+
+	// Update how much of the pattern is complete
+	current++;
+
+	// If complete, alert and reset
+	if (pattern.length === current) {
+		current = 0;
+		window.alert('Achievement Unlocked.  You are an OG.');
+    playBgm();
+	}
+
+};
+
+// related to ?
+document.addEventListener('keydown', keyHandler, false);
+
+
+// functions to play audio files
+function playSound() {
+  snd.play();
+};
+
+function playBgm() {
+  bgm.play();
+};
